c15bb57cfbe7e8d6cd8b711708fa26fcbe2a4629
Barrett <barrett@offpsite.io>
2022-08-17 18:18:37 -0600

    temp commit

--- typescript_mm_client/src/strats/stypes.ts
+++ typescript_mm_client/src/strats/stypes.ts
@@ -1,5 +1,7 @@
 
 export type MM_Hyperparams = {
-    max_size? : number
-    time_requote? : number
+    max_size?: number
+    time_requote?: number
+    spread?: number
+    order_layers?: number
 }
